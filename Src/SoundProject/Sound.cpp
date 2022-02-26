d80d258f83b2ebe642b51a6293b2097e929cca67
druiz07 <62289530+druiz07@users.noreply.github.com>
2022-02-26 12:28:11 +0100

    Añadidos metodos funcionales de control de sonido
    Co-Authored-By: Cristian Castillo de Leon <62943663+Crisca06@users.noreply.github.com>

--- Src/SoundProject/Sound.cpp
+++ Src/SoundProject/Sound.cpp
@@ -3,52 +3,63 @@
 
 SoundSystemClass::SoundSystemClass()
 {
-    if (FMOD::System_Create(&m_pSystem) != FMOD_OK)
-    { 
-        // Report Error
-        return;
-    }
-    //Manejo de drivers del ordenador para evitar que se intente reproducir cunaod no hay 
-    int driverCount = 0;
-    m_pSystem->getNumDrivers(&driverCount);
-    if (driverCount == 0)
-    {
-        // Report Error
-        return;
-    }
-    // Initialize our Instance with 36 Channels
-    m_pSystem->init(36, FMOD_INIT_NORMAL, NULL);
+	if (FMOD::System_Create(&m_pSystem) != FMOD_OK)
+	{
+		// Report Error
+		return;
+	}
+	//Manejo de drivers del ordenador para evitar que se intente reproducir cunaod no hay 
+	int driverCount = 0;
+	m_pSystem->getNumDrivers(&driverCount);
+	if (driverCount == 0)
+	{
+		// Report Error
+		return;
+	}
+	// Initialize our Instance with 36 Channels
+	m_pSystem->init(36, FMOD_INIT_NORMAL, NULL);
 }
-void SoundSystemClass::createSound(SoundClass* pSound, const char* pFile, int channel)
+void SoundSystemClass::createSound(SoundClass* pSound, const char* pFile, int channel,int mapType)
 {
-    m_pSystem->createSound(pFile, FMOD_DEFAULT, 0, pSound); //El segundo parametro antes era :_HARDWARE pero en la documentacion no etsbaa
-    //Lo unico que me cuadraba de la documentacion es el 3D aunque bastante xd la verdad
+	m_pSystem->createSound(pFile, FMOD_DEFAULT, 0, pSound); //El segundo parametro antes era :_HARDWARE pero en la documentacion no etsbaa
+	//Lo unico que me cuadraba de la documentacion es el 3D aunque bastante xd la verdad
 
-    soundsMap.insert(channel, pSound>);
+	//Habria que hacer un switch para segun añadamos sonidos a los distintos mapas ,distinguir entre mapas para hacer el insert solo una vez 
+	if(mapType==0)soundsMap.insert.insert({ channel, &pSound});
+
 }
 
 void SoundSystemClass::playSound(SoundClass pSound, bool bLoop)
 {
-    if (!bLoop)
-        pSound->setMode(FMOD_LOOP_OFF);
-    else
-    {
-        pSound->setMode(FMOD_LOOP_NORMAL);
-        pSound->setLoopCount(-1);
-    }
+	if (!bLoop)
+		pSound->setMode(FMOD_LOOP_OFF);
+	else
+	{
+		pSound->setMode(FMOD_LOOP_NORMAL);
+		pSound->setLoopCount(-1);
+	}
 
-    //Los parametros que acepta el metodo son :
-    //Sonido a Reproducir / Grupos de Canales (bastante wtf la verdad) / Si esta o no pausado / Canal por el que se reproduce (por defecto 0)
-    m_pSystem->playSound(pSound, 0, false,0); //He cambiado la sintaxis de como venia por que si no daba error
+	//Los parametros que acepta el metodo son :
+	//Sonido a Reproducir / Grupos de Canales (bastante wtf la verdad) / Si esta o no pausado / Canal por el que se reproduce (por defecto 0)
+	m_pSystem->playSound(pSound, 0, false, 0); //He cambiado la sintaxis de como venia por que si no daba error
 
 }
 
 
-void SoundSystemClass::releaseSound(SoundClass pSound)
+void SoundSystemClass::releaseSound(int map, int channel)
 {
-    pSound->release();
-}
+	if (map == 0)soundsMap.find(channel)->second->release;
+};
 
-void SoundSystemClass::pauseSound(SoundClass* pSound)
+void SoundSystemClass::setSpeed(int map, int channel,float s)
 {
+	if (map == 0)soundsMap.find(channel)->second->setMusicSpeed(s);
+
 }
+void SoundSystemClass::setVolumeChannel(int map, int channel, float volume)
+{
+	if (map == 0)soundsMap.find(channel)->second->setMusicChannelVolume(channel,volume);
+	
+}
+//Para el de pausar no encuentro como se hace con el enum del FMOD_CHANNEL
+//pero seria llamar con ese enum tanto al setPaused como al get Paused y et devuelven en el estado en el que estan
--- Src/SoundProject/Sound.h
+++ Src/SoundProject/Sound.h
@@ -8,13 +8,17 @@ typedef FMOD::Sound* SoundClass;
 class SoundSystemClass //Clase que maneja el sonido a partir del uso de FMOD
 {
 private:
-	std::unordered_map<int, SoundClass*> soundsMap;
+	std::unordered_map<int, SoundClass> soundsMap;
+	std::unordered_map<int, SoundClass> effectsMap;
+	std::unordered_map<int, SoundClass> voicesMap;
+	std::unordered_map<int, SoundClass> enviormentMap;
 public:
 	// Pointer to the FMOD instance
 	FMOD::System* m_pSystem;
 	SoundSystemClass();
-	void createSound(SoundClass* pSound, const char* pFile);
+	void createSound(SoundClass* pSound, const char* pFile,int channel, int mapType);
 	void playSound(SoundClass pSound, bool bLoop = false);
-	void releaseSound(SoundClass pSound);
-	void pauseSound(SoundClass* pSound);
+	void releaseSound(int map, int channel);
+	void setSpeed(int map, int channel,float s);
+	void setVolumeChannel(int map, int channel, float volume);
 };
