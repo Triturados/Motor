dc637e49e6fd9662964055cffd90f40a8a6f14ee
Cristian <crisca06@ucm.es>
2022-03-01 09:41:24 +0100

    Arregladas dependencias y cosas de FMOD
    Dani me ayudó mucho.
    
    Co-Authored-By: Daniel Ruiz <62289530+druiz07@users.noreply.github.com>

--- Skeleton/src/Main/main.cpp
+++ Skeleton/src/Main/main.cpp
@@ -1,8 +1,29 @@
-#include <saludador.h>
+//#include "../OgreProject/Saludador.h"
+#include <Sound.h>
+#include <iostream>
 
-int main() {
-	
-	
+void useSystemSound()
+{
+	SoundSystemClass sound = SoundSystemClass(); //Inicializacion 
 
-	return 0;
+	// Create a sample sound
+	SoundClass soundSample;
+	sound.createSound(&soundSample, "../../../Recursos/Sonidos/sonido.wav",0);
+
+	// Play the sound, with loop mode
+	sound.playSound(soundSample, true);
+
+	// Do something meanwhile...
+
+	int a;
+	std::cin >> a;
+
+	// Release the sound
+	sound.releaseSound(0);
+}
+
+void main()
+{
+	//sound("aaaa");
+	useSystemSound();
 }
