e2d4816f2d852d55c2419b9f32be8966b3ebd731
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2022-03-05 11:14:50 +0100

    errors in vector3 and class vector2

--- Skeleton/projects/Utilities/Vector3.h
+++ Skeleton/projects/Utilities/Vector3.h
@@ -32,23 +32,16 @@ public:
 		return Vector3(x + v2.x, y + v2.y, z + v2.z);
 	}
 
+	Vector3 operator-(Vector3 const& v2) const {
+		return Vector3(x - v2.x, y - v2.y, z - v2.z);
+	}
+
 	bool operator==(Vector3 const& b) const {
 		return x == v2.x && y == v2.y && z == v2.z;
 	}
 
 	bool operator!=(Vector3 const& b) const {
 		return x != v2.x || y != v2.y || z != v2.z;
-	}
-
-
-	Vector3& bool operator =(const Vector3& v1, const Vector3& v2)
-	{
-		return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-	}
-
-	Vector3 getVector()
-	{
-		return Vector3(x, y, z);
 	}
 
 	void inverse()
@@ -72,7 +65,7 @@ public:
 
 	T magnitude2() //Magnitud al cuadrado
 	{
-		return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		return pow(sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)), 2);
 	}
 
 	void normalize() //normalizar vector
