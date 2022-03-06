400a65ced0635aea188eb0dfc4235677cb9f1140
druiz07 <62289530+druiz07@users.noreply.github.com>
2022-03-06 18:29:16 +0100

    Inicio de creacion de clase Transform

--- Skeleton/src/Utilities/Transform.h
+++ Skeleton/src/Utilities/Transform.h
@@ -1,8 +1,31 @@
 #pragma once
 
 #include <vector>
+#include "Vector3.h"
+#include <Ogre.h>
+#include <OgreRenderer.h>
+#include <Component.h>
+#include <OgreSceneNode.h>
 
 class Transform :  public Component
 {
+	Vector3<float>position;
+	Vector3<float>rotation;
+	Vector3<float>scale;
+
+	Ogre::SceneNode* entityNode;
+	std::vector<Transform*> children;
+public:
+	Transform();
+	~Transform();
+	Vector3<float>getPos();
+	Vector3<float>getPos();
+	Vector3<float>getRot();
+
+	void setRot();
+	void setPos();
+	void setScale();
+
+
 };
 
