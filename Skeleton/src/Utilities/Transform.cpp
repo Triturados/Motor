70fb018aece07f3d8f1007337ae2c10e2d173dd7
druiz07 <62289530+druiz07@users.noreply.github.com>
2022-03-06 18:30:21 +0100

    Declaracion de metodos del component Transform

--- Skeleton/src/Utilities/Transform.cpp
+++ Skeleton/src/Utilities/Transform.cpp
@@ -7,3 +7,25 @@ Transform::Transform()
 Transform::~Transform()
 {
 }
+
+Vector3<float> Transform::getPos()
+{
+	return Vector3<float>();
+}
+
+Vector3<float> Transform::getRot()
+{
+	return Vector3<float>();
+}
+
+void Transform::setRot()
+{
+}
+
+void Transform::setPos()
+{
+}
+
+void Transform::setScale()
+{
+}
