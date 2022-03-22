66676d10e4091750431a1690e06aebeb52f2dab9
ivasan07 <ivasan07@ucm.es>
2022-03-22 09:13:30 +0100

    quitada build de luabridge subida por error

--- Skeleton/dependencies/LuaBridgeBuild/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ivan/Desktop/Motor/Skeleton/dependencies/LuaBridgeSrc
-# Build directory: C:/Users/Ivan/Desktop/Motor/Skeleton/dependencies/LuaBridgeBuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Source")
-subdirs("third_party/gtest")
-subdirs("Tests")
--- Skeleton/dependencies/LuaBridgeBuild/third_party/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ivan/Desktop/Motor/Skeleton/dependencies/LuaBridgeSrc/third_party/gtest/googlemock
-# Build directory: C:/Users/Ivan/Desktop/Motor/Skeleton/dependencies/LuaBridgeBuild/third_party/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
