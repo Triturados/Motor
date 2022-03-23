6b0e4248c1771afab1f84090a93c320351fac03b
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-03-23 15:13:46 +0100

    Update automate.bat

--- automate.bat
+++ automate.bat
@@ -3,56 +3,55 @@
 title Automatizacion - Proceso de compilacion de librerias
 echo Comenzando con la automatizacion!
 
-@REM rem Parte de OGRE
-@REM echo Compilando OGRE!
+rem Parte de OGRE
+echo Compilando OGRE!
 
-@REM cd Skeleton\dependencies
-@REM mkdir OgreBuild
-@REM cd CMAKE\bin
+cd Skeleton\dependencies
+mkdir OgreBuild
+cd CMAKE\bin
 
-@REM cmake -S "..\..\OgreSrc" -B "..\..\OgreBuild"
+cmake -S "..\..\OgreSrc" -B "..\..\OgreBuild"
 
-@REM cd ..\..\OgreBuild
-@REM msbuild "OGRE.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
-@REM msbuild "OGRE.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
+cd ..\..\OgreBuild
+msbuild "OGRE.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
+msbuild "OGRE.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
 
-@REM echo Compilacion de OGRE terminada!
-
-
-@REM rem Parte de Bullet
-@REM echo Compilando BULLET PHYSICS!
-
-@REM cd ..\
-
-@REM mkdir BulletBuild
-@REM cd CMAKE\bin
-
-@REM cmake -D USE_MSVC_RUNTIME_LIBRARY_DLL=TRUE -D BUILD_BULLET2_DEMOS=FALSE -D BUILD_BULLET_ROBOTICS_EXTRA=FALSE -D BUILD_BULLET_ROBOTICS_GUI_EXTRA=FALSE -D BUILD_CPU_DEMOS=FALSE -D BUILD_OPENGL3_DEMOS=FALSE -S "..\..\BulletSrc" -B "..\..\BulletBuild"
-
-@REM  cd ..\..\BulletBuild
-
-@REM msbuild "BULLET_PHYSICS.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
-@REM msbuild "BULLET_PHYSICS.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
-
-@REM echo Compilacion de BULLET PHYSICS terminada!
+echo Compilacion de OGRE terminada!
 
 
+rem Parte de Bullet
+echo Compilando BULLET PHYSICS!
 
-@REM rem Parte de SDL2
-@REM echo Compilando SDL2!
+cd ..\
 
-@REM cd ..\OgreBuild\SDL2-build
-@REM msbuild "SDL2.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
-@REM msbuild "SDL2.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
+mkdir BulletBuild
+cd CMAKE\bin
 
-@REM echo Compilacion de SDL2 terminada!
+cmake -D USE_MSVC_RUNTIME_LIBRARY_DLL=TRUE -D BUILD_BULLET2_DEMOS=FALSE -D BUILD_BULLET_ROBOTICS_EXTRA=FALSE -D BUILD_BULLET_ROBOTICS_GUI_EXTRA=FALSE -D BUILD_CPU_DEMOS=FALSE -D BUILD_OPENGL3_DEMOS=FALSE -S "..\..\BulletSrc" -B "..\..\BulletBuild"
+
+ cd ..\..\BulletBuild
+
+msbuild "BULLET_PHYSICS.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
+msbuild "BULLET_PHYSICS.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
+
+echo Compilacion de BULLET PHYSICS terminada!
+
+
+
+rem Parte de SDL2
+echo Compilando SDL2!
+
+cd ..\OgreBuild\SDL2-build
+msbuild "SDL2.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
+msbuild "SDL2.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
+
+echo Compilacion de SDL2 terminada!
 
 
 
 rem Parte de LuaBridge descargando el codigo fuente desde github
 
-rem cd ..\..\
-cd Skeleton\dependencies
+cd ..\..\
 mkdir LuaBridgeBuild
 
 cd Git\bin
@@ -66,8 +65,6 @@ cd ..\..\LuaBridgeBuild
 
 msbuild "LuaBridge.sln" /p:configuration=Debug /t:ALL_BUILD /p:Platform=x64
 msbuild "LuaBridge.sln" /p:configuration=Release /t:ALL_BUILD /p:Platform=x64
-
-call automate-LuaBridge.bat
 
 echo Compilacion de LuaBridge terminada!
 
@@ -91,21 +88,21 @@ rem Parte de LuaBridge subiendo el codigo fuente al repositorio
 
 
 rem rem Moviendo archivos (dll) necesarios (.bat individual para cada libreria)
-@REM echo Ejecutando Scripts individuales!
+echo Ejecutando Scripts individuales!
 
-@REM cd ..\Scripts
+cd ..\Scripts
 
-@REM rem OGRE
-@REM call automate-Ogre.bat
+rem OGRE
+call automate-Ogre.bat
 
-@REM rem FMOD
-@REM call automate-FMOD.bat
+rem FMOD
+call automate-FMOD.bat
 
-@REM rem LuaBridge
-@REM @REM call automate-LuaBridge.bat
+rem LuaBridge
+call automate-LuaBridge.bat
 
-@REM rem SDL2
-@REM call automate-SDL2.bat
+rem SDL2
+call automate-SDL2.bat
 
 
 echo Proceso terminado!
