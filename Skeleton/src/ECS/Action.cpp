f79016e1c76ab0f3476b9c8698f6d6aad3639d72
edekadt <edekadt@ucm.es>
2022-04-03 20:16:14 +0200

    Acciones para los agentes inteligentes

--- Skeleton/src/ECS/Action.cpp
+++ Skeleton/src/ECS/Action.cpp
@@ -1,6 +1,33 @@
 #include "Action.h"
+#include "Agent.h"
+#include <iostream>
+#include "GameTime.h"
+
 
 namespace LoveEngine {
 	namespace ECS {
+        Action::Action(Agent* agent_, float priority_)
+        {
+            agent = agent_;
+            agent->addAction(this);
+            priority = priority;
+        }
+
+        void Action::passiveUpdate()
+        {
+            addPriority(-increasePrioOverTime * LoveEngine::Time::getInstance()->deltaTime);
+        }
+
+        // Si la acci�n no se ha completado, tiene que seguir a pesar de las prioridades actuales
+        // Si las condiciones no se cumplen, no se ejecutar� esta acci�n
+        float Action::getPriority() const noexcept { return actionComplete ? LONG_MIN : conditionsFulfilled() ? priority : LONG_MAX; }
+
+        // Para incrementar la prioridad, hay que pasar valores negativos
+        void Action::addPriority(float increase) { priority += increase;}
+
+        void Action::setPriority(float priority_) {
+            priority = priority_;
+        }
+
     }
 }
--- Skeleton/src/ECS/Action.h
+++ Skeleton/src/ECS/Action.h
@@ -5,5 +5,42 @@
 
 namespace LoveEngine {
 	namespace ECS {
+        class Agent;
+        class Action
+        {
+        public:
+            // Cantidad de prioridad que se incrementa cada frame
+            float increasePrioOverTime = 0.0;
+
+            // Avisa al agente de cu�ndo se PUEDE tomar otra acci�n
+            bool actionComplete = false;
+
+            // Condiciones que se tienen que cumplir para que la acci�n se realice, ignorando prioridad
+            // Por ejemplo un ataque mel� puede requerir una m�nima proximidad al objetivo
+            virtual bool conditionsFulfilled() const { return true; };
+
+            virtual void onActionStart() {};
+
+            Action(Agent* agent_, float priority_ = LONG_MAX);
+
+            // llamado en cada frame, est� o no activa la acci�n (necesario para control de prioridad etc.)
+            virtual void passiveUpdate();
+
+            // llamado en cada frame mientras se est� ejecutando la acci�n
+            virtual void activeUpdate() {};
+
+            // Si la acci�n no se ha completado, tiene que seguir a pesar de las prioridades actuales
+            // Si las condiciones no se cumplen, no se ejecutar� esta acci�n
+            float getPriority() const noexcept;
+        protected:
+            Agent* agent;
+
+            // Para incrementar la prioridad, hay que pasar valores negativos
+            virtual void addPriority(float increase);
+            virtual void setPriority(float priority_);
+        private:
+            float priority;
+        };
+
 	}
 }
