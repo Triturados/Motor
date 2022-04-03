6ae921971cd07ccce4d7aa7a9603ae1f555d7abc
edekadt <edekadt@ucm.es>
2022-04-03 20:15:36 +0200

    Componente Agente compilable

--- Skeleton/src/ECS/Agent.cpp
+++ Skeleton/src/ECS/Agent.cpp
@@ -1,8 +1,52 @@
 #include "Agent.h"
-
+#include "Action.h"
 
 namespace LoveEngine {
 	namespace ECS {
+        Agent::Agent()
+        {
+            actions = std::vector<Action*>();
+        }
 
-	}
+        Agent::~Agent()
+        {
+            for (Action* a : actions)
+                delete a;
+        }
+
+        void Agent::update()
+        {
+            currentAction->activeUpdate();
+
+            for (Action* a : actions)
+                a->passiveUpdate();
+
+            if (currentAction->actionComplete)
+            {
+                getNextAction();
+                currentAction->onActionStart();
+            }
+        }
+
+        Action* Agent::addAction(Action* a)
+        {
+            actions.push_back(a);
+            return a;
+        }
+
+        Action* Agent::getNextAction()
+        {
+            float highestPrio = LONG_MAX;
+            
+            for (Action* a : actions)
+            {
+                if (a->getPriority() < highestPrio)
+                {
+                    currentAction = a;
+                    highestPrio = a->getPriority();
+                }
+            }
+            return currentAction;
+        }
+    }
 }
--- Skeleton/src/ECS/Agent.h
+++ Skeleton/src/ECS/Agent.h
@@ -2,12 +2,32 @@
 
 #include "Component.h"
 #include <../Export.h>
+#include <queue>
 
 namespace LoveEngine {
 	namespace ECS {
-		class lovexport Agent : public ComponentTemplate<Agent>
+        class Action;
+		class lovexport Agent : public Component
 		{
+        public:
+            Agent();
 
+            ~Agent();
+
+            void init() override {};
+
+            void update() override;
+
+            Action* addAction(Action* a);
+
+        private:
+            // vector iterable de acciones, para llamar los updates
+            std::vector<Action*> actions;
+
+            // la acción que se está realizando actualmente
+            Action* currentAction;
+
+            Action* getNextAction();
 		};
 	}
 }
