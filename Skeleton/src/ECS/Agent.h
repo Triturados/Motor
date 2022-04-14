312108258df2dc4078e7214a98c5f81d58aeee66
edekadt <edekadt@ucm.es>
2022-04-15 01:32:02 +0200

    Convertido Acción en una clase privada de Agente

--- Skeleton/src/ECS/Agent.h
+++ Skeleton/src/ECS/Agent.h
@@ -6,9 +6,107 @@
 
 namespace LoveEngine {
 	namespace ECS {
-        class Action;
+        
+        // TO DO: remove these
+        class RigidBody;
+        class Transform;
+
+
 		class lovexport Agent : public ComponentTemplate<Agent>
 		{
+
+        protected:
+#pragma region Action
+            class Action
+            {
+            public:
+                friend Agent;
+                // Cantidad de prioridad que se incrementa cada frame
+                float increasePrioOverTime = 0.0;
+
+                // Avisa al agente de que NO SE PUEDE tomar otra acción
+                bool lockAction = false;
+
+                // Condiciones que se tienen que cumplir para que la acción se realice, ignorando prioridad
+                // Por ejemplo un ataque melé puede requerir una mínima proximidad al objetivo
+                virtual bool conditionsFulfilled() const { return true; };
+
+                virtual void onActionStart() {};
+
+                Action(Agent* agent_, float priority_ = LONG_MAX);
+
+                // llamado en cada frame, esté o no activa la acción (necesario para control de prioridad etc.)
+                virtual void passiveUpdate() {};
+
+                // llamado en cada frame mientras se esté ejecutando la acción
+                virtual void activeUpdate() {};
+
+                // Si la acción no se ha completado, tiene que seguir a pesar de las prioridades actuales
+                // Si las condiciones no se cumplen, no se ejecutará esta acción
+                float getPriority() const noexcept;
+            protected:
+                Agent* agent;
+
+                // Para incrementar la prioridad, hay que pasar valores negativos
+                virtual void addPriority(float increase);
+                virtual void setPriority(float priority_);
+            private:
+                float priority;
+                // Este método es privado para evitar que sobrecargar el passiveUpdate requiera actualizar la prioridad
+                void passiveUpdateAndPrio();
+            };
+#pragma endregion
+
+#pragma region move these to game
+            class MeleeAttack : public Action
+            {
+            public:
+                MeleeAttack(Agent* agent_);
+                void setTarget(Transform* t);
+                bool conditionsFulfilled() const final;
+                void onActionStart() final;
+                void activeUpdate() final;
+            protected:
+                RigidBody* rb;
+                Transform* target = nullptr;
+                Transform* tr = nullptr;
+            };
+
+            class Idle : public Action
+            {
+            public:
+                Idle(Agent* agent_);
+                void activeUpdate() final;
+            };
+
+            class Chase : public Action
+            {
+            public:
+                Chase(Agent* agent_);
+                void setTarget(Transform* t);
+                void activeUpdate() final;
+            protected:
+                float maxVel = 10;
+                float acc = 10;
+                RigidBody* rb = nullptr;
+                Transform* target = nullptr;
+                Transform* tr = nullptr;
+            };
+
+            class Leap : public Action
+            {
+            public:
+                Leap(Agent* agent_);
+                void setTarget(Transform* t);
+                bool conditionsFulfilled() const final;
+                void onActionStart() final;
+                void activeUpdate() final;
+            protected:
+                RigidBody* rb;
+                Transform* target = nullptr;
+                Transform* tr = nullptr;
+            };
+#pragma endregion
         public:
             Agent();
 
@@ -25,7 +123,7 @@ namespace LoveEngine {
             std::vector<Action*> actions;
 
             // la acción que se está realizando actualmente
-            Action* currentAction;
+            Action* currentAction = nullptr;
 
             Action* getNextAction();
 		};
