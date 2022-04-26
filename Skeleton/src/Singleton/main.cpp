678f995546fbcc6fdc71df2410eeb443be6b8ffd
Yojhan <yojhansg@ucm.es>
2022-04-26 12:20:24 +0200

    quitado proyecto singleton

--- Skeleton/src/Singleton/main.cpp
+++ Skeleton/src/Singleton/main.cpp
@@ -1,13 +1,5 @@
 #include <Windows.h>
 #include <Singleton.h>
 
-extern "C" {
 
-	__declspec(dllexport) void* getElement(int idx) {
-		return LoveEngine::Singleton::Singleton::getValue(idx);
-	}
-	__declspec(dllexport) void createElement(void* val, int pos) {
-		LoveEngine::Singleton::Singleton::createElement(val, pos);
-	}
-}
 
--- Skeleton/src/Utilities/SingletonInfo.h
+++ Skeleton/src/Utilities/SingletonInfo.h
@@ -3,14 +3,6 @@
 namespace LoveEngine {
 	namespace Singleton {
 
-		enum class positions {
-			hola
-		};
-
-		typedef void (*singletonIN)(void*, int);
-		typedef void* (*singletonOUT)(int);
-
-		void* getElement(positions pos);
-		void  addElement(void* elem, positions pos);
+	
 	}
 }
